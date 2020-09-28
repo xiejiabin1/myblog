cd32f6f7d4fa7537883188ba094e35d915317426
xie <xie430423@163.com>
2020-09-28 09:07:12 +0800

    配置druid连接池

--- src/main/java/com/xie/myblog/util/DruidConfig.java
+++ src/main/java/com/xie/myblog/util/DruidConfig.java
@@ -1,11 +1,15 @@
 package com.xie.myblog.util;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * 数据库链接池druid监控平台的配置
@@ -14,25 +18,45 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DruidConfig {
 
+    /**
+     * 配置参数，不然application.yml中的配置不生效
+     * @return
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druid(){
+        return new DruidDataSource();
+    }
+
+    /**
+     * 监控.
+     * @return
+     */
     @Bean
     public ServletRegistrationBean druidServlet() {
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
         // IP白名单
-//        servletRegistrationBean.addInitParameter("allow", "*");
+        //servletRegistrationBean.addInitParameter("allow", "*");
         // IP黑名单(共同存在时，deny优先于allow)
-//        servletRegistrationBean.addInitParameter("deny", "127.0.0.1");
+        //servletRegistrationBean.addInitParameter("deny", "127.0.0.1");
         //控制台管理用户
         servletRegistrationBean.addInitParameter("loginUsername", "admin");
-        servletRegistrationBean.addInitParameter("loginPassword", "xiejiabin123");
+        servletRegistrationBean.addInitParameter("loginPassword", "xxgg");
         //是否能够重置数据 禁用HTML页面上的“Reset All”功能
         servletRegistrationBean.addInitParameter("resetEnable", "false");
         return servletRegistrationBean;
     }
 
+    /**
+     * 过滤器
+     * @return
+     */
     @Bean
     public FilterRegistrationBean filterRegistrationBean() {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        //添加过滤规则
         filterRegistrationBean.addUrlPatterns("/*");
+        //添加不需要忽略的格式信息
         filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
         return filterRegistrationBean;
     }
